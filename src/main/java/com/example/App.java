2cc78d9b90875abe27dd424e1c8820417b216568
uday patil <115389877+uday077@users.noreply.github.com>
2025-12-25 14:04:12 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,6 +2,17 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World! Ye AWS CodePipeline se build hua hai.");
+        System.out.println("========================================");
+        System.out.println("🚀 Hello Uday! Your Docker App is Live!");
+        System.out.println("========================================");
+        
+        // App ko turant band hone se rokne ke liye loop (Server jaisa behave karega)
+        try {
+            while (true) {
+                Thread.sleep(1000);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
